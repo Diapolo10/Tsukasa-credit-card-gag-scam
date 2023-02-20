6141e5b6b0007d7e6278be736e56938a80c95692
Lari Liuhamo <lari.liuhamo@gmail.com>
2023-02-20 17:42:34 +0200

    [PATCH] Dependabot update

--- src/script.py
+++ src/script.py
@@ -1,24 +1,31 @@
+"""Displays a fake credit card scam joke window"""
+
 import sys
 import tkinter as tk
 from pathlib import Path
 from typing import Union
 
-from PIL import Image, ImageSequence, ImageTk  # type: ignore
+from PIL import Image, ImageSequence, ImageTk
 
 
-def resource_path(relative_path: Union[str, Path]) -> Path:  #so when turning script into a exe, allows us to use other files, such as images
+FRAME_DELAY = 140  # in ms, roughly equivalent to ~7 FPS
+
+
+def resource_path(relative_path: Union[str, Path]) -> Path:
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    
+
     base_dir = Path(__file__).parent
-    
+
     if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_dir = Path(sys._MEIPASS)  # pylint: disable=no-member
+        base_dir = Path(sys._MEIPASS)  # type: ignore  # pylint: disable=no-member,protected-access
 
     return base_dir / relative_path
 
 
-def intro_text() -> None:
+def intro_text():
+    """Shows the main text label"""
+
     intro = tk.Label(
         text="H-hi there...\nDo you th-think I could have your\ncredit card information, p-please?",
         bg="#FFFFFF",
@@ -30,15 +37,31 @@ def intro_text() -> None:
     intro.grid(row=0, rowspan=1, column=1, columnspan=3)
 
 
-def text_w_entrybox(root, text_label_sentence, text_row, text_rowspan, text_column, text_columnspan, entry_row, entry_rowspan, entry_column, entry_columnspan) -> None:
+def text_w_entrybox(root: tk.Tk,
+                    text_label_sentence: str,
+                    text_row: int,
+                    text_rowspan: int,
+                    text_column: int,
+                    text_columnspan: int,
+                    entry_row: int,
+                    entry_rowspan: int,
+                    entry_column: int,
+                    entry_columnspan: int):
+    """Creates the entry box"""
+
     text_label = tk.Label(
         root,
-        text = text_label_sentence,
+        text=text_label_sentence,
         bg="#FFFFFF",
         fg="#000000",
         font=("Arial", 15),
     )
-    text_label.grid(row=text_row, rowspan=text_rowspan, column=text_column, columnspan=text_columnspan)
+    text_label.grid(
+        row=text_row,
+        rowspan=text_rowspan,
+        column=text_column,
+        columnspan=text_columnspan
+    )
 
     entry_box = tk.Entry(root, width=35)
     entry_box.grid(
@@ -51,33 +74,44 @@ def text_w_entrybox(root, text_label_sentence, text_row, text_rowspan, text_colu
 
 
 class AnimatedGIF:
+    """A wrapper for displaying GIF animations"""
+
     def __init__(self, parent, file_path):
+        """Initialises the GIF wrapper"""
+
         self.parent = parent
-        self.canvas = tk.Canvas(parent, width=278, height=286, bg="#FFFFFF") #size of image (500x286 is original gif size)
+
+        # size of the image (500x286 is the original gif size)
+        self.canvas = tk.Canvas(parent, width=278, height=286, bg="#FFFFFF")
         self.canvas.grid(row=0, rowspan=5, column=0, columnspan=1)
-        self.sequence = [
-            ImageTk.PhotoImage(img)
-            for img in ImageSequence.Iterator(
-                Image.open(file_path)
-            )
-        ]
+
+        self.sequence = []
+        with Image.open(file_path) as image_file:
+            for img in ImageSequence.Iterator(image_file):
+                self.sequence.append(ImageTk.PhotoImage(img))
 
         self.frame = 0
-        self.image = self.canvas.create_image(139, 143, image=self.sequence[self.frame]) # Make the number of this 0.5 the size of the image
 
-        # self.animate()
+        # Make the number of this 0.5 the size of the image
+        self.image = self.canvas.create_image(139, 143, image=self.sequence[self.frame])
 
     def increment_frame(self) -> None:
+        """Updates the current frame index"""
+
         self.frame = (self.frame + 1) % len(self.sequence)
 
     def animate(self) -> None:
+        """Handles the animation by re-rendering the current frame"""
+
         self.increment_frame()
-        self.parent.after(140, self.animate) # The number represents frame delay, in this case 140 ms, or ~7 FPS
+        self.parent.after(FRAME_DELAY, self.animate)
         self.canvas.itemconfig(self.image, image=self.sequence[self.frame])
 
 
-def main() -> None:
-    ### Resources
+def main():
+    """Main loop"""
+
+    # Resources
     root = tk.Tk()
     window_icon = resource_path(Path('data') / 'icon_ico.ico')
     tsukasa_gif_file = resource_path(Path('data') / 'tsukasa.gif')
@@ -92,14 +126,14 @@ def main() -> None:
         borderwidth=0
     )
 
-    ### Window Info
+    # Window Info
     root.geometry('')
-    root.title('Totally Not Malware')
+    root.title("Totally Not Malware")
     root.iconbitmap(window_icon)
     root.configure(background='#FFFFFF')
-    root.resizable(0, 0) # Disables window resizing
+    root.resizable(False, False)  # Disables window resizing
 
-    ### Main display stuff
+    # Main display stuff
     gif = AnimatedGIF(root, tsukasa_gif_file)
     gif.animate()
     intro_text()
@@ -108,8 +142,9 @@ def main() -> None:
     text_w_entrybox(root, "Security code:", 3, 1, 1, 1, 3, 1, 2, 2)
     button_quit.grid(row=4, column=2, sticky=tk.W)
 
-    ### Main loop
-    root.mainloop() #keeps the window open forever, till closed
+    # Main loop
+    root.mainloop()
+
 
 if __name__ == '__main__':
     main()
